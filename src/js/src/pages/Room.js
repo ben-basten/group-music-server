e20a33c658ee88e059f73be11e2543c58ba20108
ben-desktop <ben.basten@gmail.com>
2021-02-23 21:28:38 -0500

    Add play/pause button

--- src/js/src/pages/Room.js
+++ src/js/src/pages/Room.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {withRouter} from 'react-router-dom';
 import Header from '../components/Header';
 
@@ -7,6 +8,13 @@ function Room(props) {
   //   state: { error: `Invalid Room ID: ${props.match.params.roomId}` }
   // });
 
+  const[isPlaying, setIsPlaying] = useState(true);
+  
+  function playPause(e) {
+    e.preventDefault();
+    setIsPlaying(!isPlaying);
+  }
+
   return (
     <div className="room">
       <Header />
@@ -14,6 +22,9 @@ function Room(props) {
         <h2>Room {props.match.params.roomId}</h2>
         <p>Share this link with your friends to join!</p>
         <p>{window.location.href}</p>
+      </div>
+      <div className="media-controls">
+        <input type="button" value={isPlaying ? "Pause" : "Play"} onClick={playPause} />
       </div>
     </div>
   );
