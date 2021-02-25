fbd3bd8c44980bc5b2417eb95bd59429c2b74fb3
ben-desktop <ben.basten@gmail.com>
2021-02-25 13:38:51 -0500

    Controller with test endpoint - working!

---
+++ src/main/kotlin/com/benbasten/groupmusicserver/controller/MusicController.kt
@@ -0,0 +1,13 @@
+package com.benbasten.groupmusicserver.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MusicController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World"
+    }
+}
