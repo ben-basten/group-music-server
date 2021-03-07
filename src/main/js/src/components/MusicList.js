dc20f5b886a68b8d151a1949e63e955e1469c220
ben-desktop <ben.basten@gmail.com>
2021-03-07 11:58:38 -0500

    Handle empty list

--- src/main/js/src/components/MusicList.js
+++ src/main/js/src/components/MusicList.js
@@ -22,7 +22,7 @@ function MusicList({name, music, roomId}) {
         <div className="music-list list-group">
             <h3>{name}</h3>
             <ul>
-            {music ? music.map(track => (
+            {music && music.length > 0 ? music.map(track => (
                 <li key={track.id} className="list-group-item">
                     <div>
                         {track.title} <br/>
@@ -30,7 +30,7 @@ function MusicList({name, music, roomId}) {
                     </div>
                     <img src={QueueIcon} alt="Add to queue" onClick={() => addToQueue(track.id)} />
                 </li>
-            )) : <li>Nothing to see here... yet!</li>}
+            )) : <li className={"list-group-item"}>Nothing to see here... yet!</li>}
             </ul>
         </div>
     );
