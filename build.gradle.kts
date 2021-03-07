1088ef3b4566e34798ce66e57b3dbd21aa34c9cc
ben-desktop <ben.basten@gmail.com>
2021-03-06 20:50:51 -0500

    GROOVY BABY

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.gradle.groovy") // for Spock testing
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 }
 
 group = "com.benbasten"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -16,12 +17,20 @@ repositories {
 }
 
 dependencies {
+	val spockVersion = "1.3-groovy-2.5"
+	
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	//	Spock testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("org.spockframework:spock-core:$spockVersion")
+	testImplementation("org.spockframework:spock-spring:$spockVersion")
 }
 
 tasks.withType<KotlinCompile> {
