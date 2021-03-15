60f43ac5ecb919c3ef5b58123d37bc78b446d5d8
ben-desktop <ben.basten@gmail.com>
2021-03-15 16:57:01 -0400

    Hello endpoint test passing

--- src/test/groovy/com/benbasten/groupmusicserver/MusicControllerSpec.groovy
+++ src/test/groovy/com/benbasten/groupmusicserver/MusicControllerSpec.groovy
@@ -1,11 +1,39 @@
 package com.benbasten.groupmusicserver
 
+import com.benbasten.groupmusicserver.controller.MusicController
+import com.benbasten.groupmusicserver.service.MusicService
+import com.benbasten.groupmusicserver.service.RoomService
+import org.springframework.test.web.servlet.MockMvc
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
 import spock.lang.Specification
 
 class MusicControllerSpec extends Specification {
 
-    def 'does 1 equal 1?' () {
-        expect:
-        1 == 1
+    private MusicController musicController
+    private MusicService musicService = Mock()
+    private RoomService roomService = Mock()
+    private MockMvc mockMvc
+
+    void setup () {
+        musicController = new MusicController(musicService, roomService)
+        mockMvc = standaloneSetup(musicController).build()
+    }
+
+    def 'hello endpoint' () {
+        when:
+        def result = mockMvc
+                .perform(get("/api/gms/hello"))
+                .andExpect(status().isOk())
+                .andReturn().response
+
+        then:
+        1 * musicService.hello() >> "howdy"
+        0 * _
+
+        and:
+        result.contentAsString.contains("howdy, today is")
     }
 }
