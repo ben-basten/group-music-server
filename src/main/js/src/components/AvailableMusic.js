82fe27930f5763583e0d13535709e692dda1ca6c
ben-desktop <ben.basten@gmail.com>
2021-03-16 18:43:45 -0400

    Add search to the available music!

--- src/main/js/src/components/AvailableMusic.js
+++ src/main/js/src/components/AvailableMusic.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import QueueIcon from '../assets/icons/playlist.svg'
 
 function AvailableMusic({music, roomId, setQueue, socketClient}) {
+
+    const [filterInput, setFilterInput] = useState();
 
     const addToQueue = (trackId) => {
         fetch('/api/gms/room/queue/add', {
@@ -27,15 +30,28 @@ function AvailableMusic({music, roomId, setQueue, socketClient}) {
     return (
         <div className="music-list list-group grid-item">
             <h3 className="column-header">Available Music</h3>
+            <input
+                type="text"
+                className="filter"
+                placeholder="Filter music"
+                value={filterInput || ''}
+                onChange={event => setFilterInput(event.target.value)}
+            />
             <ul>
-            {music && music.length > 0 ? music.map(track => (
-                <li key={track.id} className="list-group-item">
-                    <div>
-                        {track.title} <br/>
-                        <small className="text-muted">{track.artist} &middot; {track.album}</small><br/>
-                    </div>
-                    <img src={QueueIcon} alt="Add to queue" onClick={() => addToQueue(track.id)} />
-                </li>
+            {music && music.length > 0 ? music
+                .filter(track => // filters the song titles/artists/albums by the user inputted filter
+                    !filterInput
+                    || track.title.toLowerCase().indexOf(filterInput.toLowerCase()) > -1
+                    || track.artist.toLowerCase().indexOf(filterInput.toLowerCase()) > -1
+                    || track.album.toLowerCase().indexOf(filterInput.toLowerCase()) > -1)
+                .map(track => (
+                    <li key={track.id} className="list-group-item">
+                        <div>
+                            {track.title} <br/>
+                            <small className="text-muted">{track.artist} &middot; {track.album}</small><br/>
+                        </div>
+                        <img src={QueueIcon} alt="Add to queue" onClick={() => addToQueue(track.id)} />
+                    </li>
             )) : null}
             </ul>
         </div>
