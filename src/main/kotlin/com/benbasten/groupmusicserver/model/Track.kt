b1e5c7c97d903d1a685438535c52bf19bfb51d49
ben-desktop <ben.basten@gmail.com>
2021-03-16 17:55:37 -0400

    Loading tracks is now bulletproof

--- src/main/kotlin/com/benbasten/groupmusicserver/model/Track.kt
+++ src/main/kotlin/com/benbasten/groupmusicserver/model/Track.kt
@@ -3,17 +3,37 @@ package com.benbasten.groupmusicserver.model
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.mpatric.mp3agic.Mp3File
 import org.springframework.core.io.Resource
+import java.lang.NullPointerException
 
 @JsonIgnoreProperties(value = ["resource"])
 class Track(val id: Int, var resource: Resource) {
     private val mp3File: Mp3File = Mp3File(resource.file)
-    val title: String = findTrackTitle()
-    val artist: String = if(mp3File.hasId3v2Tag()) mp3File.id3v2Tag.albumArtist else "Unknown artist"
-    val album: String = if(mp3File.hasId3v2Tag()) mp3File.id3v2Tag.album else "Unknown album"
+    val title: String = getTrackTitle()
+    val artist: String = getTrackArtist()
+    val album: String = getTrackAlbum()
 
-    private fun findTrackTitle(): String {
-        if(mp3File.hasId3v2Tag()) return mp3File.id3v2Tag.title
-        // use filename without extension if song title is not in metadata
-        return resource.filename!!.substringBefore('.')
+    private fun getTrackTitle(): String {
+        return try {
+            mp3File.id3v2Tag.title
+        } catch (ex: NullPointerException) {
+            // use filename without extension if song title is not in metadata
+            resource.filename!!.substringBefore('.')
+        }
+    }
+
+    private fun getTrackArtist(): String {
+        return try {
+            mp3File.id3v2Tag.albumArtist
+        } catch (ex: NullPointerException) {
+            "Unknown artist"
+        }
+    }
+
+    private fun getTrackAlbum(): String {
+        return try {
+            mp3File.id3v2Tag.album
+        } catch (ex: NullPointerException) {
+            "Unknown album"
+        }
     }
 }
