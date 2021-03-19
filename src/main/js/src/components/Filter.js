a188c9b1558a26a923b0ffdda6a30f904e6c8d0b
ben-desktop <ben.basten@gmail.com>
2021-03-19 16:23:29 -0400

    highlight filter text when clicked

--- src/main/js/src/components/Filter.js
+++ src/main/js/src/components/Filter.js
@@ -7,6 +7,7 @@ function Filter({filterInput, setFilterInput}) {
                 placeholder="Filter music"
                 value={filterInput || ''}
                 onChange={event => setFilterInput(event.target.value)}
+                onFocus={(event) => event.target.select()}
             />
             <a onClick={() => setFilterInput('')} className="ui-button">x</a>
         </div>
